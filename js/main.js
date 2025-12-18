e51fd1cee5e341fd25cb5cd84847df7fa053796c
cventuralo <cventuralo@uoc.edu>
2025-12-18 11:38:39 +0100

    Versió NYT

--- js/main.js
+++ js/main.js
@@ -1,54 +1,43 @@
 const svg = d3.select("#vis");
 const scroller = scrollama();
 
-/* ---------- NETEJA AMB TRANSICIÓ ---------- */
 function clearSVG() {
   svg.selectAll("*")
-    .transition()
-    .duration(300)
-    .style("opacity", 0)
+    .interrupt()
     .remove();
 }
 
-/* ---------- CONTROL D’ESCENES ---------- */
 function handleStepEnter(response) {
 
-  // estat actiu del text
   d3.selectAll(".step").classed("is-active", false);
   d3.select(response.element).classed("is-active", true);
 
   clearSVG();
 
-  setTimeout(() => {
-    switch (response.index) {
-      case 0:
-        drawOverview(svg);
-        break;
-      case 1:
-        showDistribution(svg);
-        break;
-      case 2:
-        drawMosaic(svg);
-        break;
-      case 3:
-        drawSankey(svg, false);
-        break;
-      case 4:
-        drawTreemap(svg);
-        break;
-      case 5:
-        drawTreemap(svg);
-        break;
-    }
-  }, 350);
+  switch (response.index) {
+    case 0:
+      drawOverview(svg);
+      break;
+    case 1:
+      showDistribution(svg);
+      break;
+    case 2:
+      drawMosaic(svg);
+      break;
+    case 3:
+      drawSankey(svg, false);
+      break;
+    case 4:
+      drawTreemap(svg);
+      break;
+  }
 }
 
-/* ---------- INIT ---------- */
 function init() {
   scroller
     .setup({
       step: ".step",
-      offset: 0.55
+      offset: 0.5
     })
     .onStepEnter(handleStepEnter);
 
@@ -57,5 +46,5 @@ function init() {
 
 window.addEventListener("load", init);
 
-/* ---------- ESCENA INICIAL ---------- */
+// escena inicial
 drawOverview(svg);
