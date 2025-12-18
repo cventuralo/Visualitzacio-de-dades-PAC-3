6c8f7f304e3e7ef881fad39f968d1fd31547e6bd
cventuralo <cventuralo@uoc.edu>
2025-12-18 09:18:43 +0100

    v2 commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,36 @@
 const svg = d3.select("#vis");
 const scroller = scrollama();
 
-scroller
-  .setup({
-    step: ".step",
-    offset: 0.6
-  })
-  .onStepEnter(response => {
-    switch (response.index) {
-      case 0:
-        drawOverview(svg);
-        break;
-      case 1:
-        showDistribution(svg);
-        break;
-      case 2:
-        drawSankey(svg, false);
-        break;
-      case 3:
-        drawSankey(svg, true);
-        break;
-    }
-  });
+function handleStepEnter(response) {
+  switch (response.index) {
+    case 0:
+      drawOverview(svg);
+      break;
+    case 1:
+      showDistribution(svg);
+      break;
+    case 2:
+      drawSankey(svg, false);
+      break;
+    case 3:
+      drawSankey(svg, true);
+      break;
+  }
+}
+
+function init() {
+  scroller
+    .setup({
+      step: ".step",
+      offset: 0.6
+    })
+    .onStepEnter(handleStepEnter);
+
+  window.addEventListener("resize", scroller.resize);
+}
+
+// 🔥 AIXÒ ÉS OBLIGATORI
+window.addEventListener("load", init);
 
 // escena inicial
 drawOverview(svg);
-
